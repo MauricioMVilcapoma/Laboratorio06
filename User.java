fcec9823d01364c40661b61fab296c0ee7e57595
Ramiro Guzmán <130616535+RamiroGuzmanCh@users.noreply.github.com>
2023-04-26 22:31:22 -0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,11 @@
+package SistemaDeVuelos;
+
+import java.util.List;
+
+public interface User {
+       void registerUser(User user);
+       void removeUser(User user);
+       void notifyUsers();
+	
+
+}
